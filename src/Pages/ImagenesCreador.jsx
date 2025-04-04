a03fc072f58faf47b0541fbfcd15026335dad481
javier-prieto-uwu <2302097@utrivieramaya.edu.mx>
2025-04-03 22:00:51 -0500

    dsfkjk

--- src/Pages/ImagenesCreador.jsx
+++ src/Pages/ImagenesCreador.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import "./Imagenes_creador.css"; // Importa el archivo CSS actualizado
+
 
 export default function CivitaiImagesCreador({ limite = 200 }) {
   const [images, setImages] = useState([]);
